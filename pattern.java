e9b03ec332695032dbf9ff326ab2fcef0a6e2b17
Prajwal Kote <127095366+WhiteDevil-PRK@users.noreply.github.com>
2023-03-06 15:14:16 +0530

    java program

---
+++ pattern.java
@@ -0,0 +1,11 @@
+
+    
+
+
+ class  PatternExample{
+  public static void main(String args[])
+  System.out.println ("*");
+  System.out.println ("& &");
+  System.out.println (" % % %");
+  System.out.println ("# # # #");
+}
